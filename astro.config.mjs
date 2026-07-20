df723a2b4dc87d283d8a1d322ce4f589b62a266d
Speedmarketing-dev <aldinhyuseinov@gmail.com>
2026-07-20 22:26:09 +0300

    Update navigation links to use dynamic base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+// @ts-ignore
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 // https://astro.build/config
 export default defineConfig({
   // 1. Set your custom domain or GitHub pages domain (without subfolders)
   site: "https://speedmarketing-dev.github.io",
 
   // 2. Set the exact name of your GitHub repository (with leading slash)
-  base: "/restaurant-starata-kashta",
+  base: isGithubActions ? "/restaurant-starata-kashta" : "/",
   vite: {
     plugins: [tailwindcss()],
   },
