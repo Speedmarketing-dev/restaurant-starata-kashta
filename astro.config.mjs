ff8b186e265c3ab9c1c3a3aea4f4e0db070235ff
Speedmarketing-dev <aldinhyuseinov@gmail.com>
2026-07-20 22:00:18 +0300

    Refactor astro.config.mjs for consistency and clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Set your custom domain or GitHub pages domain (without subfolders)
+  site: "https://speedmarketing-dev.github.io",
+
+  // 2. Set the exact name of your GitHub repository (with leading slash)
+  base: "/restaurant-starata-kashta",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
